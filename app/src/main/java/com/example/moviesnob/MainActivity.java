c1e9036bad644ae66f9911c2df9629353f317f36
nareshban1 <nareshban@users.noreply.github.com>
2019-04-29 19:21:13 +0545

    c

--- app/src/main/java/com/example/moviesnob/MainActivity.java
+++ app/src/main/java/com/example/moviesnob/MainActivity.java
@@ -26,10 +26,10 @@ public class MainActivity extends AppCompatActivity
 
 
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar =  findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer =  findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
@@ -39,14 +39,17 @@ public class MainActivity extends AppCompatActivity
 
 
 
-        TextView login= (TextView) findViewById(R.id.nav_log);
+        TextView login=  findViewById(R.id.nav_log);
+
+        //yo code le kaam garena error dincha
         /*login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(getApplicationContext(), Login.class));
             }
         });*/
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+
+        NavigationView navigationView =  findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
 
@@ -63,7 +66,7 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer =  findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
@@ -94,7 +97,7 @@ public class MainActivity extends AppCompatActivity
         else if (id == R.id.nav_log) {
             startActivity(new Intent(getApplicationContext(), Login.class));
         }
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer =  findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
