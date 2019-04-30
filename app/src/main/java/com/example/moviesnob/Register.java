9cc60a6ba63a8b2a12b8333649666dc4dbc96ad9
nareshban1 <nareshban@users.noreply.github.com>
2019-04-30 13:50:26 +0545

    c

--- app/src/main/java/com/example/moviesnob/Register.java
+++ app/src/main/java/com/example/moviesnob/Register.java
@@ -22,6 +22,8 @@ import com.google.firebase.database.FirebaseDatabase;
 public class Register extends AppCompatActivity {
     private EditText email;
     private EditText uname;
+    private EditText fname;
+    private EditText lname;
     private EditText pass;
     private Button register;
     private TextView rlogin;
@@ -58,6 +60,8 @@ public class Register extends AppCompatActivity {
                 final String mEmail=email.getText().toString().trim();
                 String mPassword= pass.getText().toString().trim();
                 final String mUsername= uname.getText().toString().trim();
+                final String mFname= fname.getText().toString().trim();
+                final String mLname= lname.getText().toString().trim();
 
                 if(TextUtils.isEmpty(mEmail)){
                     email.setError("Required");
@@ -78,7 +82,7 @@ public class Register extends AppCompatActivity {
                         if(task.isSuccessful()){
 
 
-                            User user= new User(mUsername, mEmail);
+                            User user= new User(mUsername, mEmail,mFname,mLname);
                             FirebaseDatabase.getInstance().getReference("Users").child(FirebaseAuth.getInstance().getCurrentUser().getUid())
                                     .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
                                 @Override
