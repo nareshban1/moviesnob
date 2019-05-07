ec70d4b2ae4ba65a3b1668920640b3b14f185980
nareshban1 <nareshban@users.noreply.github.com>
2019-05-07 15:40:36 +0545

    on click

--- app/src/main/java/com/example/moviesnob/model/Movie.java
+++ app/src/main/java/com/example/moviesnob/model/Movie.java
@@ -4,14 +4,20 @@ public class Movie {
 
     private String Title;
     private String image_url;
+    private String back_url;
+    private String description;
+    private String rating;
 
     public Movie(){
 
     }
-    public Movie(String Title, String image_url){
+    public Movie(String Title, String image_url, String description, String rating, String back_url){
 
         this.Title =Title;
         this.image_url = image_url;
+        this.description = description;
+        this.rating = rating;
+        this.back_url=back_url;
 
     }
 
@@ -30,5 +36,29 @@ public class Movie {
 
     public void setImage_url(String image_url) {
         this.image_url = image_url;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public String getBack_url() {
+        return "https://image.tmdb.org/t/p/w500" + back_url;
+    }
+
+    public void setBack_url(String back_url) {
+        this.back_url = back_url;
     }
 }
